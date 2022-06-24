53b04cd052d554ab02eeb9d2192f42e37f7f80eb
Leandro Nazareth <52218718+leandrosnazareth@users.noreply.github.com>
2022-06-24 10:47:25 -0300

    add produtos na tabela ok

--- src/app/page/sale/productSold.ts
+++ src/app/page/sale/productSold.ts
@@ -3,12 +3,14 @@ export class ProductSold {
     id: number;
     product: any;
     price: any;
+    priceTotal: any;
     quantity: number;
 
-    constructor(id: number, product: any, price: any, quantity: number) {
+    constructor(id: number, product: any, price: any, priceTotal: any, quantity: number) {
         this.id = id;
         this.product = product;
         this.price = price;
+        this.priceTotal = priceTotal;
         this.quantity = quantity;
     }
 }
--- src/app/page/sale/sale.component.ts
+++ src/app/page/sale/sale.component.ts
@@ -13,17 +13,17 @@ import { Sale } from './sale';
 export class SaleComponent implements OnInit {
 
   productsSolds: ProductSold[] = [];
-  formulario: FormGroup;
+  formAddItem: FormGroup;
   //lista de products para exiboir
   sales: Sale[] = [];
   //ordem das colunas no html 
-  ordemColunasTabela = ['id', 'quantity', 'name', 'price'];
+  ordemColunasTabela = ['id', 'quantity', 'name', 'price', 'priceTotal'];
   totalElementos = 0;
   pagina = 0;
   tamanho = 5;
   pageSizeOptions: number[] = [5, 10, 15, 100]; // [10,20,30] quantidade de item por página
   mensagemErros: String[] = []; //array de strings dos erros retornados do backend
-  
+
   constructor(
     private productService: ProductService,
     private formBilder: FormBuilder,
@@ -36,25 +36,33 @@ export class SaleComponent implements OnInit {
   }
 
   montarFormulario() {
-    this.formulario = this.formBilder.group({
+    this.formAddItem = this.formBilder.group({
       //validando os dados do formulário
       id: [null, [Validators.required, Validators.maxLength(13), Validators.minLength(1)]],
       quantidade: [null, [Validators.required, Validators.maxLength(50)]],
     })
   }
 
-  submit() { }
+  submit() {
+
+  }
 
   adicionarItem() {
-    const formValues = this.formulario.value;
+    const formValues = this.formAddItem.value;
     this.productService.findProductById(formValues.id).subscribe((response) => {
-      const productSold = new ProductSold(0, response, response.price, formValues.quantidade);
+      const total = response.price * formValues.quantidade;
+      const productSold = new ProductSold(
+        0,
+        response,
+        response.price,
+        total,
+        formValues.quantidade
+      );
       this.productsSolds = this.productsSolds.concat(productSold);
     })
   }
 
   getTotalCost() {
-    return this.productsSolds.map(t => t.price.valor).reduce((acc, value) => acc + value, 0);
+    return this.productsSolds.map(t => t.priceTotal).reduce((acc, value) => acc + value, 0);
   }
-
 }
